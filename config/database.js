7517e87a003e6516a50e892c30a2c3be21a76baa
Dishon <dishonodavis@gmail.com>
2019-07-28 19:38:10 -0400

    first commit

--- config/database.js
+++ config/database.js
@@ -1,6 +1,14 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://demo:demo@cluster0-92dvh.mongodb.net/test?retryWrites=true', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'dbName': 'demo'
+    'url' : 'mongodb+srv://resilient:fproject@fitness-on5e6.mongodb.net/getFit?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'resilient'
 };
+
+
+    //cluster name = fitness
+    //mongodb = resilient
+    //mongodb password = fproject
+    //dbName =
+    //dbpassword =
+    //collection name = savage_prp
--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 var thumbUp = document.getElementsByClassName("fa-thumbs-up");
-var trash = document.getElementsByClassName("fa-trash");
+var trash = document.getElementsByClassName("throwAway");
+var thumbDown = document.getElementsByClassName("fa fa-thumbs-down");
 
 Array.from(thumbUp).forEach(function(element) {
       element.addEventListener('click', function(){
@@ -25,18 +26,45 @@ Array.from(thumbUp).forEach(function(element) {
       });
 });
 
-Array.from(trash).forEach(function(element) {
+Array.from(thumbDown).forEach(function(element) {
       element.addEventListener('click', function(){
+        console.log("thumbDown", thumbDown)
         const name = this.parentNode.parentNode.childNodes[1].innerText
         const msg = this.parentNode.parentNode.childNodes[3].innerText
+        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        fetch('countDown', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            'thumbUp':thumbUp
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        alert("pug")
+        console.log("Hello")
+        const name = this.parentNode.parentNode.childNodes[1].innerText // Leon this is Not targeting the parent node properly so it wont delete
+        const msg = this.parentNode.parentNode.childNodes[3].getAttribute('data-name')
+        console.log(msg)
         fetch('messages', {
           method: 'delete',
           headers: {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify({
-            'name': name,
-            'msg': msg
+            'name': name
           })
         }).then(function (response) {
           window.location.reload()
